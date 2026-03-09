5efec771491d81c9e0044b0f890df393c7a9bf30
Prorammer-4090 <aiyesanoladimeji@gmail.com>
2026-03-09 00:03:37 -0300

    Added resources and other setup files

---
+++ include/mesh.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "glad/glad.h"
+#include <glm/glm.hpp>
+#include "shader.h"
+
+
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+struct Texture {
+    unsigned int id;
+    std::string type;
+    std::string path;  // store the path of the texture to compare with other textures
+};
+
+class Mesh {
+    public:
+        // mesh data
+        std::vector<Vertex>       vertices;
+        std::vector<unsigned int> indices;
+        std::vector<Texture>      textures;
+
+        Mesh();
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+        void Draw(Shader &shader);
+        
+        // Getters for instanced rendering
+        unsigned int getVAO() const { return VAO; }
+        unsigned int getIndexCount() const { return static_cast<unsigned int>(indices.size()); }
+
+    private:
+        // render data
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh();
+};
